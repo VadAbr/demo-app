e0a1708b1dedb5b2e459dddfc51c09db851ea6d5
Abramov Vadim <vadimabramov322@gmail.com>
2024-03-10 12:45:11 +0300

    add gh pages

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { App, AppStore } from './app';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
